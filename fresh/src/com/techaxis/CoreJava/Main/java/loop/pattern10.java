83529842ca163f65fca85d621148fb8c5eff0a4d
amish00 <menastla8@gmail.com>
2022-04-29 15:19:25 +0545

    addfile

--- fresh/src/com/techaxis/CoreJava/Main/java/loop/pattern10.java
+++ fresh/src/com/techaxis/CoreJava/Main/java/loop/pattern10.java
@@ -3,13 +3,18 @@ package com.techaxis.CoreJava.Main.java.loop;
 public class pattern10 {
     public static void main(String[] args) {
         char c=70;
-        for(int i = 1; i<6; i++) {
-            for (int j = 1; j < 7-i; j++) {
+        int i,j,k;
+        for( i = 1; i<6; i++) {
+
+            for ( j = 1; j < 7-i; j++) {
                 System.out.print(" ");
             }
-            for(int k=1;k<=i;k++){
-                System.out.print(c+" ");
+            for(int h=0;h<i;h++){
                 c--;
+            }
+            for( k=1;k<=i;k++){
+                System.out.print(c+" ");
+              c++;
             }
             c=70;
             System.out.println("  ");
--- fresh/src/com/techaxis/CoreJava/Main/java/loop/pattern8.java
+++ fresh/src/com/techaxis/CoreJava/Main/java/loop/pattern8.java
@@ -1,5 +1,22 @@
 package com.techaxis.CoreJava.Main.java.loop;
 
 public class pattern8 {
+    public static void main(String[] args) {
+        int i,j,k,a=6;
+        for( i = 1; i<6; i++) {
 
+            for ( j = 1; j < 7-i; j++) {
+                System.out.print(" ");
+            }
+            for(int h=0;h<i;h++){
+                a--;
+            }
+            for( k=1;k<=i;k++){
+                System.out.print(a+" ");
+                a++;
+            }
+            a=6;
+            System.out.println("  ");
+        }
+    }
 }
