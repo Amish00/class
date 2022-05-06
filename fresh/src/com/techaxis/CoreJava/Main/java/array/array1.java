bba55eb3f1789c9b1b96ed8a7ce7b10a8d24f01b
amish00 <menastla8@gmail.com>
2022-05-06 15:16:52 +0545

    addfile

--- fresh/src/com/techaxis/CoreJava/Main/java/array/array1.java
+++ fresh/src/com/techaxis/CoreJava/Main/java/array/array1.java
@@ -1,7 +1,56 @@
 package com.techaxis.CoreJava.Main.java.array;
+import java.util.Scanner;
+
 
 public class array1 {
     public static void main(String[] args) {
+        ;
+        Scanner scanfObj = new Scanner(System.in);
+        int a[][] = new int[3][3];
+        int b[][] = new int[3][3];
+        int sum[][] = new int[3][3];
+        int i, j;
+        System.out.println("Enter A");
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                a[i][j] = scanfObj.nextInt();
+            }
+        }
+        System.out.println("Enter B");
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                b[i][j] = scanfObj.nextInt();
+            }
+        }
+        System.out.println("matrix A");
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                System.out.print(a[i][j] + "\t ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("matrix B");
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                System.out.print(b[i][j] + "\t ");
+            }
+            System.out.println();
+        }
+
+        for (i=0;i<3;i++) {
+            for (j = 0; j < 3; j++) {
+                sum[i][j]=a[i][j]+b[i][j];
+            }
+        }
+
+        System.out.println("matrix A+B ");
+        for (i=0;i<3;i++) {
+            for (j = 0; j < 3; j++) {
+                System.out.print(sum[i][j] + "\t ");
+            }
+            System.out.println();
+        }
 
     }
 }
